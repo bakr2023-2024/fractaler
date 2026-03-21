dd99f5c4f2c79d880429e27e96a81d4c57ddf28b
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-03-21 17:55:37 +0200

    fixed power method of Complex

--- utils.hpp
+++ utils.hpp
@@ -13,13 +13,35 @@ public:
         Complex num = Complex{x, y} * Complex{oth.x, -oth.y};
         return {num.x / denom, num.y / denom};
     }
-    Complex operator^(const Complex &oth) const { return pow(oth.x, oth.y); }
+    Complex operator^(const Complex &oth) const { return power(oth.x, oth.y); }
     double mag() const { return sqrt(x * x + y * y); }
     double mag2() const { return x * x + y * y; }
-    Complex pow(double x, double y) const
+    Complex power(double a, double b = 0) const
     {
+        if (b == 0)
+        {
+            if (floor(a) == a)
+            {
+                int n = a;
+                Complex res = {1, 0};
+                Complex base = {x, y};
+                while (n > 0)
+                {
+                    if (n % 2 == 1)
+                        res = res * base;
+                    base = base * base;
+                    n /= 2;
+                }
+                return res;
+            }
+            double rn = pow(mag(), a);
+            double theta = atan2(y, x);
+            return {rn * cos(a * theta), rn * sin(a * theta)};
+        }
         double r = mag();
-        double e = exp(x);
-        return Complex{r * e * cos(y), r * e * sin(y)};
+        double theta = atan2(y, x);
+        double mul = pow(r, a) * exp(-b * theta);
+        double imag = a * theta + b * log(r);
+        return {mul * cos(imag), mul * sin(imag)};
     }
 };
