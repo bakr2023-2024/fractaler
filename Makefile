ecd17092636fea7f08fde17e22d7f2997d4d5d77
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-04-05 02:57:04 +0200

    tested parsing of Polynomial class

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
+.PHONY: clean
 main: main.cpp fractaler.cpp
 	g++ --std=c++20 -o main main.cpp fractaler.cpp -lraylib -lopengl32 -lgdi32 -lwinmm -fopenmp
-utils-test: test/utils.cpp utils.cpp
-	g++ --std=c++20 -o utils-test test/utils.cpp utils.cpp
+utils-test: test/utils.cpp
+	g++ --std=c++20 -o utils-test test/utils.cpp
+clean:
+	rm -rf *.exe
--- test/utils.cpp
+++ test/utils.cpp
@@ -36,20 +36,36 @@ TEST_CASE("Testing Complex class")
         CHECK_LT(mag, 5.84);
     }
 }
+void checkComplex(const Complex &complex, double cx, double cy)
+{
+    CHECK_EQ(complex.x, cx);
+    CHECK_EQ(complex.y, cy);
+}
 TEST_CASE("Testing PolynomialTerm class")
 {
     PolynomialTerm term{-2, 3, 2};
     SUBCASE("Testing substitution")
     {
-        Complex res = term.substitute({4, 2});
-        CHECK_EQ(res.x, -72);
-        CHECK_EQ(res.y, 4);
+        checkComplex(term.substitute({4, 2}), -72, 4);
     }
     SUBCASE("Testing differentiation")
     {
+
         PolynomialTerm diff = term.differentiate(2);
-        CHECK_EQ(diff.coeff.x, -4);
-        CHECK_EQ(diff.coeff.y, 6);
+        checkComplex(diff.coeff, -4, 6);
         CHECK_EQ(diff.pow, 0);
+    }
+}
+TEST_CASE("Testing Polynomial class")
+{
+    Polynomial poly{"(2 - 3i)z^2 - (-4 + 2i)z + (13 + 5i)"};
+    SUBCASE("Testing parsing")
+    {
+        checkComplex(poly.terms[0].coeff, 2, -3);
+        CHECK_EQ(poly.terms[0].pow, 2);
+        checkComplex(poly.terms[1].coeff, 4, -2);
+        CHECK_EQ(poly.terms[1].pow, 1);
+        checkComplex(poly.terms[2].coeff, 13, 5);
+        CHECK_EQ(poly.terms[2].pow, 0);
     }
 }
